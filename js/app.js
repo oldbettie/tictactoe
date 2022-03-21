53fc9842fd1676b3784ddc451b1ee53986c9b0e6
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-03-21 13:09:09 +0800

    blank board

--- js/app.js
+++ js/app.js
@@ -10,10 +10,9 @@ const players = [
 		score: 0,
 		character: "o",
 	},
-];
-// figure out which players turn it is
+]; // ---start positions
 let player = players[0];
-
+let board = [];
 //once they click a sq change turns
 const changePlayer = function () {
 	if (player === players[0]) {
@@ -22,20 +21,69 @@ const changePlayer = function () {
 	} else player = players[0];
 	return player;
 };
-// update display tbf
-// const render = function() {
-//  // render screen updates here
 
-// }
 // target individual squares
 $(".board-0-0").click(function () {
 	$(".board-0-0").text(player.character);
+	$(".board-0-0").addClass("taken");
 	changePlayer(player);
 });
 $(".board-0-1").click(function () {
 	$(".board-0-1").text(player.character);
+	$(".board-0-1").addClass("taken");
+	changePlayer(player);
+});
+$(".board-0-2").click(function () {
+	$(".board-0-2").text(player.character);
+	$(".board-0-2").addClass("taken");
 	console.log(player.character);
+	changePlayer(player);
+});
+$(".board-1-0").click(function () {
+	$(".board-1-0").text(player.character);
+	$(".board-1-0").addClass("taken");
+	console.log(player.character);
+	changePlayer(player);
+});
+$(".board-1-1").click(function () {
+	$(".board-1-1").text(player.character);
+	$(".board-1-1").addClass("taken");
+	changePlayer(player);
+});
+$(".board-1-2").click(function () {
+	$(".board-1-2").text(player.character);
+	$(".board-1-2").addClass("taken");
+	changePlayer(player);
+});
+$(".board-2-0").click(function () {
+	$(".board-2-0").text(player.character);
+	$(".board-2-0").addClass("taken");
+	changePlayer(player);
+});
+$(".board-2-1").click(function () {
+	$(".board-2-1").text(player.character);
+	$(".board-2-1").addClass("taken");
+	changePlayer(player);
+});
+$(".board-2-2").click(function () {
+	$(".board-2-2").text(player.character);
+	$(".board-2-2").addClass("taken");
 	changePlayer(player);
 });
 
 //check for end of game
+const updateBoard = function () {};
+
+const makeNewBoard = function (x, y) {
+	for (let i = 0; i < x; i++) {
+		board.push([]);
+		for (let j = 0; j < y; j++) {
+			board[i][j] = "";
+		}
+	}
+
+	return board;
+};
+
+makeNewBoard(3, 3);
+console.log(board);
